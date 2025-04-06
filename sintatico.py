4be8458a988b0b3965b4cf0d9641977a01edde8c
Matheus Parizotto <matheusparizotto29@gmail.com>
2025-04-06 14:55:07 -0400

    Continuando desenvolvimento do sintatico

--- sintatico.py
+++ sintatico.py
@@ -7,3 +7,37 @@ class AnalisadorSintatico:
         if self.posicao < len(self.tokens):
             return self.tokens[self.posicao]
         return None
+    
+    def consumir(self, tipo_esperado):
+        token = self.token_atual()
+        if token and token[0] == tipo_esperado:
+            self.posicao += 1
+        else:
+            raise SyntaxError(f"Erro Sintático: Esperado '{tipo_esperado}', encontrado '{token}'")
+
+    def analisar(self):
+        self.programa()
+        print("Análise sintática concluída com sucesso!")
+
+    def programa(self):
+        self.consumir('PUBLIC')
+        self.consumir('CLASS')
+        self.consumir('ID')
+        self.consumir('ABRE_CHAVE')
+        self.consumir('PUBLIC')
+        self.consumir('STATIC')
+        self.consumir('VOID')
+        self.consumir('MAIN')
+        self.consumir('PARENTESE_ESQ')
+        self.consumir('STRING')
+        self.consumir('ABRE_COLCHETE')
+        self.consumir('FECHA_COLCHETE')
+        self.consumir('ID')
+        self.consumir('PARENTESE_DIR')
+        self.consumir('ABRE_CHAVE')
+        self.comandos()
+        self.consumir('FECHA_CHAVE')
+        self.consumir('FECHA_CHAVE')
+
+    def comandos(self):
+        pass
